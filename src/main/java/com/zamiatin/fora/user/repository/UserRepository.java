20d14f51e215e8755582a6c74d652ea195bfcec3
Igor Zamiatin <capra.lanigera@gmail.com>
2025-05-25 08:35:16 +0300

    Remove gradle

--- src/main/java/com/zamiatin/fora/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zamiatin.fora.user.repository;
-
-import com.zamiatin.fora.user.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
